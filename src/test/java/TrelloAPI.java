b7221c4f2cd9dcea570545da9e859db3c97bfc2c
Sameh <sameh.s.n.hanna@gmail.com>
2023-01-10 20:38:01 +0200

    Create empty Trello workspace

--- src/test/java/TrelloAPI.java
+++ src/test/java/TrelloAPI.java
@@ -1,5 +1,18 @@
+import io.restassured.RestAssured;
+
 public class TrelloAPI {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+
+        RestAssured
+                .given()
+                .baseUri("https://api.trello.com/1/organizations")
+                .queryParam("displayName", "FWD")
+                .queryParam("key", "982abc8dcdc1f5c1f521158a8a130b2e")
+                .queryParam("token", "ATTA21ffc751290b3358369e166609b32df8553dfb06ba83ffb998a84cf6e0277df8210993B3")
+                .body("")
+                .when()
+                .post()
+                .prettyPrint();
     }
 }
